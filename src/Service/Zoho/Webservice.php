0531541a9d99317bbd2c380d15c201418a32faa2
Noud de Brouwer <noud@webdesigntilburg.nl>
2019-04-11 09:30:40 +0000

    Throw error iso echo.

--- src/Service/Zoho/Webservice.php
+++ src/Service/Zoho/Webservice.php
@@ -2,6 +2,7 @@
 
 namespace App\Service\Zoho;
 
+use Symfony\Component\Debug\Exception\FatalThrowableError;
 use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
 use Symfony\Component\Filesystem\Filesystem;
 
@@ -88,7 +89,7 @@ abstract class Webservice
                 $filesystem->touch($this->logPath.'/ZCRMClientLibrary.log');
                 $tokenPersistenceFileCreated = true;
             } catch (IOExceptionInterface $exception) {
-                echo 'An error occurred while creating your file at '.$exception->getPath();
+                throw new FatalThrowableError('An error occurred while creating your file at '.$exception->getPath());
             }
         }
         // sometimes i need
