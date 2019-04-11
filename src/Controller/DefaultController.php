459c3f6038e63b3b31166411321e13374e50da68
Noud de Brouwer <noud@webdesigntilburg.nl>
2019-04-11 09:40:26 +0000

    Remove unneeded suppresswarning.

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,9 +6,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @SuppressWarnings(PMD.CouplingBetweenObjects)
- */
 class DefaultController extends AbstractController
 {
     /**
