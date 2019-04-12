884cfb2f7c3f067a8967d38fba7a7f6c6c1bf0e6
Noud de Brouwer <noud@webdesigntilburg.nl>
2019-04-12 14:42:49 +0000

    Do not use nelmio/alice.

---
+++ src/DataFixtures/PageFixtures.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Page;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class PageFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $page = new Page();
+        $page->setSlug('/register');
+        $page->setTitle('Account aanmaken');
+        $page->setMetaTitle('Create Account');
+        $page->setContent('Hier komt tekst te staan over de werkwijze van het aanmaken van een account. Deze moet te beheren zijn.');
+        $manager->persist($page);
+
+        $page = new Page();
+        $page->setSlug('/register-thanks');
+        $page->setTitle('Bedankt voor je registratie');
+        $page->setMetaTitle('Thanks creating your Account');
+        $page->setContent('Hier komt tekst te staan.');
+        $manager->persist($page);
+        
+        $manager->flush();
+    }
+}
