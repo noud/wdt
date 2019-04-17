bb155808ed8458585e511729e493d310a8bfbbb5
Noud de Brouwer <noud@webdesigntilburg.nl>
2019-04-17 12:57:57 +0000

    Curl error handling and timeouts.

--- src/Service/Zoho/ZohoApiService.php
+++ src/Service/Zoho/ZohoApiService.php
@@ -42,9 +42,24 @@ class ZohoApiService
             'Content-Type: application/x-www-form-urlencoded;charset=UTF-8',
             'Authorization: Zoho-oauthtoken '.$this->zohoAccessTokenService->getAccessToken(),
         ]);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 0);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 400); //timeout in seconds
+
         /** @var string $result */
         $result = curl_exec($ch);
-        $result = json_decode($result);
+        if ($errorNumber = curl_errno($ch)) {
+            if (\in_array($errorNumber, [CURLE_OPERATION_TIMEDOUT, CURLE_OPERATION_TIMEOUTED], true)) {
+                curl_close($ch);
+                throw new \Exception('timeout..in getRequest..');
+            }
+        }
+
+        try {
+            $result = json_decode($result);
+        } catch (\Exception $e) {
+            curl_close($ch);
+            throw new \Exception('json decode error..in getRequest..');
+        }
 
         if (57 === $result->code) {
             // @TODO check refresh the token..
@@ -53,7 +68,11 @@ class ZohoApiService
             //$this->getRequest($url);
             // @TODO and i should keep a timer about how many times..
             // now how do i recall this getRequest function?
+            curl_close($ch);
             throw new \Exception('refresh the token..in getRequest..');
+        } elseif (0 !== $result->code) {
+            curl_close($ch);
+            throw new \Exception('Error occured..in getRequest..');
         }
 
         return $result;
