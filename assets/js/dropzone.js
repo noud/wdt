167e03c22f0fab4b00c8af2c9ea2b810a4e8ddfe
Noud de Brouwer <noud@webdesigntilburg.nl>
2019-04-25 09:18:59 +0000

    File size limit.

--- assets/js/dropzone.js
+++ assets/js/dropzone.js
@@ -1,7 +1,7 @@
 import $ from 'jquery';
 import Dropzone from 'dropzone';
 
-//$(document).ready(function() {
+$(document).ready(function() {
     let urlPost = document.getElementById('urls').getAttribute('data-attachment-post');
     let urlRemove = document.getElementById('urls').getAttribute('data-attachment-remove');
     
@@ -16,7 +16,7 @@ import Dropzone from 'dropzone';
 	        data: frm.serialize(),
 	    });
 	});
-	var myDropzone = new Dropzone("div#offer-dropzone", { url: urlPost, addRemoveLinks: true});
+	var myDropzone = new Dropzone("div#ticket-dropzone", { url: urlPost, addRemoveLinks: true});
 	myDropzone.on("sending", function(file, xhr, formData) {
 		  formData.append("filename", file.name);
 		  formData.append("filesize", file.size);
@@ -28,4 +28,10 @@ import Dropzone from 'dropzone';
 	        data: {'filename': file.name},
 	    });
 	});
-//});
+	myDropzone.on("addedfile", function(file) {
+		if(file.size > (1024 * 1024 * 20)) // not more than 20mb
+		{
+			this.removeFile(file); // if you want to remove the file or you can add alert or presentation of a message
+		}
+	});
+});
