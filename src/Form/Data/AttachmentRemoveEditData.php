04ed42e35eecbdd59eab02857937bdc493e4a04f
Noud de Brouwer <noud@webdesigntilburg.nl>
2019-05-08 12:35:30 +0000

    NotBlank.

--- src/Form/Data/AttachmentRemoveEditData.php
+++ src/Form/Data/AttachmentRemoveEditData.php
@@ -2,10 +2,13 @@
 
 namespace App\Form\Data;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 class AttachmentRemoveEditData
 {
     /**
      * @var string
+     * @Assert\NotBlank()
      */
     public $filename;
 }
