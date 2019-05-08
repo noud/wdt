afc547bad2d9165a9dbe449f9a208ab6e1214120
Noud de Brouwer <noud@webdesigntilburg.nl>
2019-05-08 12:32:40 +0000

    Translate message.

--- src/Controller/TicketLocalAttachmentController.php
+++ src/Controller/TicketLocalAttachmentController.php
@@ -10,9 +10,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class TicketLocalAttachmentController extends AbstractController
 {
+    /**
+     * @var TranslatorInterface
+     */
+    private $translator;
+    
+    public function __construct(
+        TranslatorInterface $translator
+    ) {
+        $this->translator = $translator;
+    }
+    
     /**
      * Remove a local attachment, that's not uploaded to Zoho but saved locally until the ticket is created.
      * @Route("/ticket/attachment/remove", name="ticket_local_attachment_remove")
@@ -27,6 +39,6 @@ class TicketLocalAttachmentController extends AbstractController
             return new Response('', 200);
         }
 
-       throw $this->createNotFoundException('Attachment not exists');
+        throw $this->createNotFoundException($this->translator->trans('attachment.message.file_not_exist', [], 'attachment'));
     }
 }
