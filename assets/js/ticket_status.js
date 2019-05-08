7e1a17a2f6fef66b55933460cea54c10a50a4d1c
Noud de Brouwer <noud@webdesigntilburg.nl>
2019-05-08 09:06:30 +0000

    Do not import unused jquery.

--- assets/js/ticket_status.js
+++ assets/js/ticket_status.js
@@ -1,5 +1,3 @@
-import $ from 'jquery';
-
 $(document).ready(function() {
     document.getElementById('ticket_status_status').addEventListener("change", function(evt){
     	document.getElementById('ticket_status').submit();
