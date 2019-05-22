513c7aae42556025beccd135f240314eff6164b1
Noud de Brouwer <noud@webdesigntilburg.nl>
2019-05-22 13:48:03 +0000

    Use names like used inside CURLFile.

--- src/Zoho/Service/Desk/TicketAttachmentService.php
+++ src/Zoho/Service/Desk/TicketAttachmentService.php
@@ -49,19 +49,19 @@ class TicketAttachmentService
         return $publicTicketAttachments;
     }
 
-    public function createTicketAttachment(string $file, int $ticketId, string $fileName = null): array
+    public function createTicketAttachment(string $name, int $ticketId, string $postname = null): array
     {
         $organisationId = $this->organizationService->getOrganizationId();
 
         /** @var string $fileMime */
-        $fileMime = mime_content_type($file);
-        if (!$fileName) {
-            $fileName = basename($file);
+        $mime = mime_content_type($name);
+        if (!$postname) {
+            $postname = basename($name);
         }
 
         $data = [
             'isPublic' => 'true',
-            'file' => new \CURLFile($file, $fileMime, $fileName),
+            'file' => new \CURLFile($name, $mime, $postname),
         ];
 
         return $this->zohoApiService->post('tickets/'.$ticketId.'/attachments', $organisationId, [], $data);
